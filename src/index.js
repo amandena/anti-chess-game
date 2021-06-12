4acefc70f66073c3886c3cc3ab872385e50a2137
amandena <manda.m.johnson@gmail.com>
2021-06-11 21:54:43 -0400

    added a title to App

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+const title = 'Anti-Chess!'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title={title}/>
   </React.StrictMode>,
   document.getElementById('root')
 )
