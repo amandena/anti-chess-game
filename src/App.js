6b64f9e2a573c75950cdbf7100d245739bd113b5
amandena <manda.m.johnson@gmail.com>
2021-06-14 23:59:19 -0400

    commented out imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css'
 // import '@babel/polyfill'
-import './App.css'
 import { Provider } from 'react-redux'
 // import store from '@redux/store'
 import AntiChess from './components/AntiChess'
